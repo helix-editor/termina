154e920e9613627f3d3eab6c0acb0b7ee84b7aaa
Michael Davis <mcarsondavis@gmail.com>
2025-04-03 15:56:54 -0400

    Add support for SGR codes

--- src/escape/csi.rs
+++ src/escape/csi.rs
@@ -1,5 +1,7 @@
 use std::fmt::{self, Display};
 
+use crate::style::{Blink, ColorSpec, Font, Intensity, RgbaColor, Underline, VerticalAlign};
+
 pub(crate) const ENTER_ALTERNATE_SCREEN: Csi = Csi::Mode(Mode::SetDecPrivateMode(
     DecPrivateMode::Code(DecPrivateModeCode::ClearAndEnableAlternateScreen),
 ));
@@ -11,6 +13,9 @@ pub(crate) const EXIT_ALTERNATE_SCREEN: Csi = Csi::Mode(Mode::ResetDecPrivateMod
 // TODO: no Copy?
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Csi {
+    /// "Set Graphics Rendition" (SGR).
+    /// These sequences affect how the cell is rendered by the terminal.
+    Sgr(Sgr),
     Mode(Mode),
     Keyboard(Keyboard),
     // TODO...
@@ -21,9 +26,135 @@ impl Display for Csi {
         // This here is the "control sequence introducer" (CSI):
         write!(f, "\x1b[")?;
         match self {
+            Self::Sgr(sgr) => sgr.fmt(f),
             Self::Mode(mode) => mode.fmt(f),
             Self::Keyboard(keyboard) => keyboard.fmt(f),
         }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Sgr {
+    /// Resets the graphics rendition to default.
+    Reset,
+    Intensity(Intensity),
+    Underline(Underline),
+    Blink(Blink),
+    Italic(bool),
+    Inverse(bool),
+    Invisible(bool),
+    StrikeThrough(bool),
+    Overline(bool),
+    Font(Font),
+    VerticalAlign(VerticalAlign),
+    Foreground(ColorSpec),
+    Background(ColorSpec),
+    UnderlineColor(ColorSpec),
+}
+
+impl Display for Sgr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fn format_true_color(
+            code: u8,
+            RgbaColor {
+                red,
+                green,
+                blue,
+                alpha,
+            }: RgbaColor,
+            f: &mut fmt::Formatter,
+        ) -> fmt::Result {
+            if alpha == 255 {
+                write!(f, "{code}:2::{red}:{green}:{blue}")
+            } else {
+                write!(f, "{code}:6::{red}:{green}:{blue}:{alpha}")
+            }
+        }
+
+        // CSI <n> m
+        match self {
+            Self::Reset => write!(f, "0")?,
+            Self::Intensity(Intensity::Normal) => write!(f, "22")?,
+            Self::Intensity(Intensity::Bold) => write!(f, "1")?,
+            Self::Intensity(Intensity::Dim) => write!(f, "2")?,
+            Self::Underline(Underline::None) => write!(f, "24")?,
+            Self::Underline(Underline::Single) => write!(f, "4")?,
+            Self::Underline(Underline::Double) => write!(f, "21")?,
+            Self::Underline(Underline::Curly) => write!(f, "4:3")?,
+            Self::Underline(Underline::Dotted) => write!(f, "4:4")?,
+            Self::Underline(Underline::Dashed) => write!(f, "4:5")?,
+            Self::Blink(Blink::None) => write!(f, "25")?,
+            Self::Blink(Blink::Slow) => write!(f, "5")?,
+            Self::Blink(Blink::Rapid) => write!(f, "6")?,
+            Self::Italic(true) => write!(f, "3")?,
+            Self::Italic(false) => write!(f, "23")?,
+            Self::Inverse(true) => write!(f, "7")?,
+            Self::Inverse(false) => write!(f, "27")?,
+            Self::Invisible(true) => write!(f, "8")?,
+            Self::Invisible(false) => write!(f, "28")?,
+            Self::StrikeThrough(true) => write!(f, "9")?,
+            Self::StrikeThrough(false) => write!(f, "29")?,
+            Self::Overline(true) => write!(f, "53")?,
+            Self::Overline(false) => write!(f, "55")?,
+            Self::Font(Font::Default) => write!(f, "10")?,
+            Self::Font(Font::Alternate(1)) => write!(f, "11")?,
+            Self::Font(Font::Alternate(2)) => write!(f, "12")?,
+            Self::Font(Font::Alternate(3)) => write!(f, "13")?,
+            Self::Font(Font::Alternate(4)) => write!(f, "14")?,
+            Self::Font(Font::Alternate(5)) => write!(f, "15")?,
+            Self::Font(Font::Alternate(6)) => write!(f, "16")?,
+            Self::Font(Font::Alternate(7)) => write!(f, "17")?,
+            Self::Font(Font::Alternate(8)) => write!(f, "18")?,
+            Self::Font(Font::Alternate(9)) => write!(f, "19")?,
+            Self::Font(_) => (),
+            Self::VerticalAlign(VerticalAlign::BaseLine) => write!(f, "75")?,
+            Self::VerticalAlign(VerticalAlign::SuperScript) => write!(f, "73")?,
+            Self::VerticalAlign(VerticalAlign::SubScript) => write!(f, "74")?,
+            Self::Foreground(ColorSpec::Reset) => write!(f, "39")?,
+            Self::Foreground(ColorSpec::BLACK) => write!(f, "30")?,
+            Self::Foreground(ColorSpec::RED) => write!(f, "31")?,
+            Self::Foreground(ColorSpec::GREEN) => write!(f, "32")?,
+            Self::Foreground(ColorSpec::YELLOW) => write!(f, "33")?,
+            Self::Foreground(ColorSpec::BLUE) => write!(f, "34")?,
+            Self::Foreground(ColorSpec::MAGENTA) => write!(f, "35")?,
+            Self::Foreground(ColorSpec::CYAN) => write!(f, "36")?,
+            Self::Foreground(ColorSpec::WHITE) => write!(f, "37")?,
+            Self::Foreground(ColorSpec::BRIGHT_BLACK) => write!(f, "90")?,
+            Self::Foreground(ColorSpec::BRIGHT_RED) => write!(f, "91")?,
+            Self::Foreground(ColorSpec::BRIGHT_GREEN) => write!(f, "92")?,
+            Self::Foreground(ColorSpec::BRIGHT_YELLOW) => write!(f, "93")?,
+            Self::Foreground(ColorSpec::BRIGHT_BLUE) => write!(f, "94")?,
+            Self::Foreground(ColorSpec::BRIGHT_MAGENTA) => write!(f, "95")?,
+            Self::Foreground(ColorSpec::BRIGHT_CYAN) => write!(f, "96")?,
+            Self::Foreground(ColorSpec::BRIGHT_WHITE) => write!(f, "97")?,
+            Self::Foreground(ColorSpec::PaletteIndex(idx)) => write!(f, "38:5:{idx}")?,
+            Self::Foreground(ColorSpec::TrueColor(color)) => format_true_color(38, *color, f)?,
+            Self::Background(ColorSpec::Reset) => write!(f, "49")?,
+            Self::Background(ColorSpec::BLACK) => write!(f, "40")?,
+            Self::Background(ColorSpec::RED) => write!(f, "41")?,
+            Self::Background(ColorSpec::GREEN) => write!(f, "42")?,
+            Self::Background(ColorSpec::YELLOW) => write!(f, "43")?,
+            Self::Background(ColorSpec::BLUE) => write!(f, "44")?,
+            Self::Background(ColorSpec::MAGENTA) => write!(f, "45")?,
+            Self::Background(ColorSpec::CYAN) => write!(f, "46")?,
+            Self::Background(ColorSpec::WHITE) => write!(f, "47")?,
+            Self::Background(ColorSpec::BRIGHT_BLACK) => write!(f, "100")?,
+            Self::Background(ColorSpec::BRIGHT_RED) => write!(f, "101")?,
+            Self::Background(ColorSpec::BRIGHT_GREEN) => write!(f, "102")?,
+            Self::Background(ColorSpec::BRIGHT_YELLOW) => write!(f, "103")?,
+            Self::Background(ColorSpec::BRIGHT_BLUE) => write!(f, "104")?,
+            Self::Background(ColorSpec::BRIGHT_MAGENTA) => write!(f, "105")?,
+            Self::Background(ColorSpec::BRIGHT_CYAN) => write!(f, "106")?,
+            Self::Background(ColorSpec::BRIGHT_WHITE) => write!(f, "107")?,
+            Self::Background(ColorSpec::PaletteIndex(idx)) => write!(f, "48:5:{idx}")?,
+            Self::Background(ColorSpec::TrueColor(color)) => format_true_color(48, *color, f)?,
+            Self::UnderlineColor(ColorSpec::Reset) => write!(f, "59")?,
+            Self::UnderlineColor(ColorSpec::PaletteIndex(idx)) => write!(f, "58:5:{idx}")?,
+            Self::UnderlineColor(ColorSpec::TrueColor(color)) => {
+                format_true_color(58, *color, f)?;
+            }
+        }
+        write!(f, "m")
     }
 }
 
@@ -298,11 +429,12 @@ mod test {
     use super::*;
     #[test]
     fn encoding() {
+        // Enter the alternate screen using the mode part of CSI.
         // <https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#alternate-screen-buffer>
         assert_eq!("\x1b[?1049h", ENTER_ALTERNATE_SCREEN.to_string());
         assert_eq!("\x1b[?1049l", EXIT_ALTERNATE_SCREEN.to_string());
 
-        // Kitty keyboard flags used by Helix and Kakoune at time of writing
+        // Push Kitty keyboard flags used by Helix and Kakoune at time of writing.
         assert_eq!(
             "\x1b[>5u",
             Csi::Keyboard(Keyboard::PushFlags(
@@ -310,6 +442,17 @@ mod test {
                     | KittyKeyboardFlags::REPORT_ALTERNATE_KEYS
             ))
             .to_string()
+        );
+
+        // Common SGR: turn the text (i.e. foreground) green
+        assert_eq!(
+            "\x1b[32m",
+            Csi::Sgr(Sgr::Foreground(ColorSpec::GREEN)).to_string(),
+        );
+        // ... and then reset to turn off the green.
+        assert_eq!(
+            "\x1b[39m",
+            Csi::Sgr(Sgr::Foreground(ColorSpec::Reset)).to_string(),
         );
     }
 }
--- src/style.rs
+++ src/style.rs
@@ -2,7 +2,6 @@
 
 /// Styling of a cell's underline.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
-#[repr(u8)]
 // <https://sw.kovidgoyal.net/kitty/underlines/>
 pub enum Underline {
     /// No underline
@@ -18,6 +17,69 @@ pub enum Underline {
     Dotted = 4,
     /// Dashed underline
     Dashed = 5,
+}
+
+/// An 8-bit "256-color".
+///
+/// Colors 0-15 are the same as `AnsiColor`s (0-7 being normal colors and 8-15 being "bright").
+/// Colors 16-231 make up a 6x6x6 "color cube." The remaining 232-255 colors define a
+/// dark-to-light grayscale in 24 steps.
+///
+/// These are also known as "web-safe colors" or "X11 colors" historically, although the actual
+/// colors varied somewhat between historical usages.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// <https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit>
+pub struct WebColor(pub u8);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct RgbColor {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+}
+
+impl RgbColor {
+    pub const fn new(red: u8, green: u8, blue: u8) -> Self {
+        Self { red, green, blue }
+    }
+
+    /// The floats are expected to be in the range `0.0..=1.0`.
+    pub fn new_f32(red: f32, green: f32, blue: f32) -> Self {
+        let red = (red * 255.) as u8;
+        let green = (green * 255.) as u8;
+        let blue = (blue * 255.) as u8;
+        Self { red, green, blue }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct RgbaColor {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+    /// Also known as "opacity"
+    pub alpha: u8,
+}
+
+impl From<RgbaColor> for RgbColor {
+    fn from(color: RgbaColor) -> Self {
+        Self {
+            red: color.red,
+            green: color.green,
+            blue: color.blue,
+        }
+    }
+}
+
+impl From<RgbColor> for RgbaColor {
+    fn from(color: RgbColor) -> Self {
+        Self {
+            red: color.red,
+            green: color.green,
+            blue: color.blue,
+            alpha: 255,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -42,43 +104,88 @@ pub enum AnsiColor {
     BrightWhite,
 }
 
-/// A 256 "XTerm" color.
-///
-/// See <https://en.wikipedia.org/wiki/X11_color_names>.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct X11Color(pub u8);
+pub type PaletteIndex = u8;
 
-/// 24-bit "true color" colors.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct TrueColor {
-    pub red: u8,
-    pub green: u8,
-    pub blue: u8,
+pub enum ColorSpec {
+    Reset,
+    PaletteIndex(PaletteIndex),
+    TrueColor(RgbaColor),
 }
 
-impl TrueColor {
-    pub const fn new(red: u8, green: u8, blue: u8) -> Self {
-        Self { red, green, blue }
-    }
+impl ColorSpec {
+    pub const BLACK: Self = Self::PaletteIndex(AnsiColor::Black as PaletteIndex);
+    pub const RED: Self = Self::PaletteIndex(AnsiColor::Red as PaletteIndex);
+    pub const GREEN: Self = Self::PaletteIndex(AnsiColor::Green as PaletteIndex);
+    pub const YELLOW: Self = Self::PaletteIndex(AnsiColor::Yellow as PaletteIndex);
+    pub const BLUE: Self = Self::PaletteIndex(AnsiColor::Blue as PaletteIndex);
+    pub const MAGENTA: Self = Self::PaletteIndex(AnsiColor::Magenta as PaletteIndex);
+    pub const CYAN: Self = Self::PaletteIndex(AnsiColor::Cyan as PaletteIndex);
+    pub const WHITE: Self = Self::PaletteIndex(AnsiColor::White as PaletteIndex);
+    pub const BRIGHT_BLACK: Self = Self::PaletteIndex(AnsiColor::BrightBlack as PaletteIndex);
+    pub const BRIGHT_RED: Self = Self::PaletteIndex(AnsiColor::BrightRed as PaletteIndex);
+    pub const BRIGHT_GREEN: Self = Self::PaletteIndex(AnsiColor::BrightGreen as PaletteIndex);
+    pub const BRIGHT_YELLOW: Self = Self::PaletteIndex(AnsiColor::BrightYellow as PaletteIndex);
+    pub const BRIGHT_BLUE: Self = Self::PaletteIndex(AnsiColor::BrightBlue as PaletteIndex);
+    pub const BRIGHT_MAGENTA: Self = Self::PaletteIndex(AnsiColor::BrightMagenta as PaletteIndex);
+    pub const BRIGHT_CYAN: Self = Self::PaletteIndex(AnsiColor::BrightCyan as PaletteIndex);
+    pub const BRIGHT_WHITE: Self = Self::PaletteIndex(AnsiColor::BrightWhite as PaletteIndex);
+}
 
-    /// The floats are expected to be in the range `0.0..=1.0`.
-    pub fn new_f32(red: f32, green: f32, blue: f32) -> Self {
-        let red = (red * 255.) as u8;
-        let green = (green * 255.) as u8;
-        let blue = (blue * 255.) as u8;
-        Self { red, green, blue }
-    }
-
-    pub const fn get_rgb(&self) -> (u8, u8, u8) {
-        (self.red, self.green, self.blue)
+impl From<AnsiColor> for ColorSpec {
+    fn from(color: AnsiColor) -> Self {
+        Self::PaletteIndex(color as u8)
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Color {
-    Ansi(AnsiColor),
-    X11(X11Color),
-    TrueColor(TrueColor),
+impl From<WebColor> for ColorSpec {
+    fn from(color: WebColor) -> Self {
+        Self::PaletteIndex(color.0)
+    }
+}
+
+impl From<RgbColor> for ColorSpec {
+    fn from(color: RgbColor) -> Self {
+        Self::TrueColor(color.into())
+    }
+}
+
+impl From<RgbaColor> for ColorSpec {
+    fn from(color: RgbaColor) -> Self {
+        Self::TrueColor(color)
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub enum Intensity {
+    #[default]
+    Normal,
+    Bold,
+    Dim,
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub enum Blink {
+    #[default]
+    None,
+    Slow,
+    Rapid,
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub enum Font {
+    #[default]
+    Default,
+    /// An alternate font. Valid values are 1-9.
+    Alternate(u8),
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub enum VerticalAlign {
+    #[default]
+    BaseLine = 0,
+    SuperScript = 1,
+    SubScript = 2,
 }
 
 /*
