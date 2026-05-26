c04c2c767273f57225cbd5c45cc12f4b357618dd
Michael Davis <mcarsondavis@gmail.com>
2026-05-26 09:12:54 -0400

    Fix Kitty Keyboard `Modifiers` bitflags
    The Kitty keyboard spec supports caps_lock and num_lock as well. The
    main bug here was a copy-paste where HYPER AND META were both shifted
    to the same value (`1 << 5`). This change fixes both issues. We also
    switch to using the same numerical values as the spec, both for
    symmetry with the spec but also to keep the value within a u8.
    
    https://sw.kovidgoyal.net/kitty/keyboard-protocol/#modifiers

--- src/event.rs
+++ src/event.rs
@@ -83,12 +83,15 @@ bitflags::bitflags! {
     #[derive(Debug, Clone, Copy, PartialEq, Eq)]
     pub struct Modifiers: u8 {
         const NONE = 0;
-        const SHIFT = 1 << 1;
-        const ALT = 1 << 2;
-        const CONTROL = 1 << 3;
-        const SUPER = 1 << 4;
-        const HYPER = 1 << 5;
+        const SHIFT = 1;
+        const ALT = 1 << 1;
+        const CONTROL = 1 << 2;
+        /// Windows/Linux key or Command key on Macs.
+        const SUPER = 1 << 3;
+        const HYPER = 1 << 4;
         const META = 1 << 5;
+        const CAPS_LOCK = 1 << 6;
+        const NUM_LOCK = 1 << 7;
     }
 }
 
